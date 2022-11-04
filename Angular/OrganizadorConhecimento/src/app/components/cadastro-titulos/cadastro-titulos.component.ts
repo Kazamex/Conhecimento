79f089078b3e1164588a552b1037b0a5d5b21b82
Tadao Kazama <robertotadaokazama@gmail.com>
2022-11-04 17:59:20 -0300

    Navegação cadastro-titulos

--- Angular/OrganizadorConhecimento/src/app/components/cadastro-titulos/cadastro-titulos.component.ts
+++ Angular/OrganizadorConhecimento/src/app/components/cadastro-titulos/cadastro-titulos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-titulos',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroTitulosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  voltar() {
+    this.router.navigate(['titulos']);
+  }
 }
